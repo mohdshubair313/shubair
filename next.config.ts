af260af0c8ee33047a0b14036749fce583b635af
mohdshubair313 <shubair313@gmail.com>
2025-04-10 19:23:19 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
-import createMDX from '@next/mdx'
- 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-}
- 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
- 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+  experimental: {
+    mdxRs: true, // enable the built-in Rust MDX support
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+};
+
+export default nextConfig;
